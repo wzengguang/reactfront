25dbb24788dac0487dfa24ef370ce6c4d42e43e9
wenbo dong <rafaeld@wicresoft.com>
2022-08-11 17:56:11 +0800

    sync

--- src/WicresoftBBS.Api/Program.cs
+++ src/WicresoftBBS.Api/Program.cs
@@ -1,31 +1,43 @@
 using Microsoft.EntityFrameworkCore;
 using WicresoftBBS.Api.Models;
+using WicresoftBBS.Api.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace WicresoftBBS.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //WicresoftForumContext
+            var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+            // Add services to the container.
+            builder.Services.AddAuthorization();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddControllers();
+            builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<BBSDbContext>(
+            builder.Services.AddDbContext<BBSDbContext>(
                 options => options.UseSqlServer(builder.Configuration.GetConnectionString("WicresoftBBSDatabase")));
 
-var app = builder.Build();
+            builder.Services.AddScoped<IBBSRepo, BBSRepo>();
+            var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ src/WicresoftBBS.Api/Services/BBSRepo.cs
@@ -0,0 +1,39 @@
+﻿using WicresoftBBS.Api.Models;
+
+namespace WicresoftBBS.Api.Services
+{
+    public class BBSRepo : IBBSRepo
+    {
+        private BBSDbContext _context;
+        public BBSRepo(BBSDbContext wicresoftForumContext)
+        {
+            _context = wicresoftForumContext;
+        }
+
+        public Task<int> AddUsers(IList<User> users)
+        {
+            _context.Users.AddRange(users);
+            return _context.SaveChangesAsync();
+        }
+
+        public User GetUser(int id)
+        {
+            return _context.Users.Where(user=>user.Id == id).First();
+        }
+
+        public List<User> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        public Task<int> ReFillUsers(IList<User> users)
+        {
+            foreach (var user in _context.Users.ToList())
+            {
+                _context.Users.Remove(user);
+            }
+            _context.Users.AddRange(users);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
