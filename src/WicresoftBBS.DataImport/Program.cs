ae763e71280bc8d9724f3642ad559b03b53c52ed
Wenbo dong <rafaeld@wicresoft.com>
2022-08-11 18:27:04 +0800

    Add DataImport

---
+++ src/WicresoftBBS.DataImport/Program.cs
@@ -0,0 +1,13 @@
+﻿using WicresoftBBS.DataImport.BasicImport;
+
+namespace WicresoftBBS.DataImport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            BasicDataImporter.UserImport();
+        }
+    }
+}
